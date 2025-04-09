d59df5dd4f497f5a0a6531caea0016030cf867fa
Kyran <kyransolomon5@gmail.com>
2025-04-09 16:36:32 +0800

    new admin project

--- cashierform/Form1.Designer.vb
+++ cashierform/Form1.Designer.vb
@@ -27,24 +27,34 @@ Partial Class Form1
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(266, 203)
+        Button1.Location = New Point(149, 100)
+        Button1.Margin = New Padding(2, 2, 2, 2)
         Button1.Name = "Button1"
-        Button1.Size = New Size(112, 34)
+        Button1.Size = New Size(321, 149)
         Button1.TabIndex = 0
-        Button1.Text = "Button1"
+        Button1.Text = "Cashier to"
         Button1.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(640, 360)
         Controls.Add(Button1)
+        Margin = New Padding(2, 2, 2, 2)
         Name = "Form1"
-        Text = "Form1"
+        Text = "Cashier Form"
         ResumeLayout(False)
     End Sub
 
     Friend WithEvents Button1 As Button
 
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
 End Class
