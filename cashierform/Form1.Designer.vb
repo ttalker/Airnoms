66ea9f22c99b2121e818b3885f6adb136c32769b
Kyran <kyransolomon5@gmail.com>
2025-04-15 11:09:22 +0800

    changes in cashier

--- cashierform/Form1.Designer.vb
+++ cashierform/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class formCashierBooking
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,26 +22,88 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        lblMain = New Label()
+        lblMainBooking = New Label()
+        btnBooking = New Button()
+        btnTicket = New Button()
+        pnlleftInfo = New Panel()
+        pnlRightInfo = New Panel()
+        btnClear = New Button()
+        btnBook = New Button()
         SuspendLayout()
         ' 
-        ' lblMain
+        ' lblMainBooking
         ' 
-        lblMain.BackColor = SystemColors.ActiveCaption
-        lblMain.Location = New Point(27, 78)
-        lblMain.Name = "lblMain"
-        lblMain.Size = New Size(1359, 725)
-        lblMain.TabIndex = 0
+        lblMainBooking.BackColor = SystemColors.ActiveCaption
+        lblMainBooking.Location = New Point(30, 92)
+        lblMainBooking.Name = "lblMainBooking"
+        lblMainBooking.Size = New Size(1490, 792)
+        lblMainBooking.TabIndex = 0
         ' 
-        ' Form1
+        ' btnBooking
+        ' 
+        btnBooking.Location = New Point(30, 17)
+        btnBooking.Name = "btnBooking"
+        btnBooking.Size = New Size(194, 58)
+        btnBooking.TabIndex = 1
+        btnBooking.Text = "BOOKING"
+        btnBooking.UseVisualStyleBackColor = True
+        ' 
+        ' btnTicket
+        ' 
+        btnTicket.Location = New Point(244, 16)
+        btnTicket.Name = "btnTicket"
+        btnTicket.Size = New Size(194, 58)
+        btnTicket.TabIndex = 2
+        btnTicket.Text = "TICKET"
+        btnTicket.UseVisualStyleBackColor = True
+        ' 
+        ' pnlleftInfo
+        ' 
+        pnlleftInfo.Location = New Point(60, 144)
+        pnlleftInfo.Name = "pnlleftInfo"
+        pnlleftInfo.Size = New Size(744, 699)
+        pnlleftInfo.TabIndex = 3
+        ' 
+        ' pnlRightInfo
+        ' 
+        pnlRightInfo.Location = New Point(843, 144)
+        pnlRightInfo.Name = "pnlRightInfo"
+        pnlRightInfo.Size = New Size(648, 620)
+        pnlRightInfo.TabIndex = 4
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(1083, 785)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(194, 58)
+        btnClear.TabIndex = 5
+        btnClear.Text = "CLEAR"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnBook
+        ' 
+        btnBook.Location = New Point(1297, 785)
+        btnBook.Name = "btnBook"
+        btnBook.Size = New Size(194, 58)
+        btnBook.TabIndex = 7
+        btnBook.Text = "BOOK"
+        btnBook.UseVisualStyleBackColor = True
+        ' 
+        ' formCashierBooking
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1413, 833)
-        Controls.Add(lblMain)
+        ClientSize = New Size(1545, 910)
+        Controls.Add(btnBook)
+        Controls.Add(btnClear)
+        Controls.Add(pnlRightInfo)
+        Controls.Add(pnlleftInfo)
+        Controls.Add(btnTicket)
+        Controls.Add(btnBooking)
+        Controls.Add(lblMainBooking)
         Margin = New Padding(2)
-        Name = "Form1"
-        Text = "Cashier Form"
+        Name = "formCashierBooking"
+        Text = " "
         ResumeLayout(False)
     End Sub
 
@@ -54,5 +116,11 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents lblMain As Label
+    Friend WithEvents lblMainBooking As Label
+    Friend WithEvents btnBooking As Button
+    Friend WithEvents btnTicket As Button
+    Friend WithEvents pnlleftInfo As Panel
+    Friend WithEvents pnlRightInfo As Panel
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnBook As Button
 End Class
