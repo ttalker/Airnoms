cb3e82f86eaee5574f43a4d1c21f0cc2fe6ab033
Kyran <kyransolomon5@gmail.com>
2025-04-19 21:21:34 +0800

    Login form

--- Airnoms/Form1.vb
+++ Airnoms/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
-
+    Private Sub btnCreateAccount_Click(sender As Object, e As EventArgs) Handles btnCreateAccount.Click
+        Me.Hide()
+        Form2.Show()
+    End Sub
 End Class
