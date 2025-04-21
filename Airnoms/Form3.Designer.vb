122836e7cd9c75d5f578c6939568cd3019f5793f
Kyran <kyransolomon5@gmail.com>
2025-04-21 09:02:08 +0800

    form 3 and 4 of userform

--- Airnoms/Form3.Designer.vb
+++ Airnoms/Form3.Designer.vb
@@ -22,17 +22,42 @@ Partial Class Form3
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        btnBookingUser = New Button()
+        btnHomeUser = New Button()
         SuspendLayout()
+        ' 
+        ' btnBookingUser
+        ' 
+        btnBookingUser.Location = New Point(241, 21)
+        btnBookingUser.Name = "btnBookingUser"
+        btnBookingUser.Size = New Size(194, 58)
+        btnBookingUser.TabIndex = 5
+        btnBookingUser.Text = "BOOKING"
+        btnBookingUser.UseVisualStyleBackColor = True
+        ' 
+        ' btnHomeUser
+        ' 
+        btnHomeUser.Location = New Point(27, 22)
+        btnHomeUser.Name = "btnHomeUser"
+        btnHomeUser.Size = New Size(194, 58)
+        btnHomeUser.TabIndex = 4
+        btnHomeUser.Text = "HOME"
+        btnHomeUser.UseVisualStyleBackColor = True
         ' 
         ' Form3
         ' 
         AutoScaleDimensions = New SizeF(120F, 120F)
         AutoScaleMode = AutoScaleMode.Dpi
         ClientSize = New Size(1545, 910)
+        Controls.Add(btnBookingUser)
+        Controls.Add(btnHomeUser)
         MaximumSize = New Size(1563, 957)
         Name = "Form3"
         StartPosition = FormStartPosition.CenterScreen
         Text = "Form3"
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents btnBookingUser As Button
+    Friend WithEvents btnHomeUser As Button
 End Class
--- Airnoms/Form4.Designer.vb
+++ Airnoms/Form4.Designer.vb
@@ -22,17 +22,53 @@ Partial Class Form4
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        btnTicket = New Button()
+        btnBooking = New Button()
+        lblMainBooking = New Label()
         SuspendLayout()
+        ' 
+        ' btnTicket
+        ' 
+        btnTicket.Location = New Point(241, 21)
+        btnTicket.Name = "btnTicket"
+        btnTicket.Size = New Size(194, 58)
+        btnTicket.TabIndex = 5
+        btnTicket.Text = "TICKET"
+        btnTicket.UseVisualStyleBackColor = True
+        ' 
+        ' btnBooking
+        ' 
+        btnBooking.Location = New Point(27, 22)
+        btnBooking.Name = "btnBooking"
+        btnBooking.Size = New Size(194, 58)
+        btnBooking.TabIndex = 4
+        btnBooking.Text = "BOOKING"
+        btnBooking.UseVisualStyleBackColor = True
+        ' 
+        ' lblMainBooking
+        ' 
+        lblMainBooking.BackColor = SystemColors.ActiveCaption
+        lblMainBooking.Location = New Point(27, 97)
+        lblMainBooking.Name = "lblMainBooking"
+        lblMainBooking.Size = New Size(1490, 792)
+        lblMainBooking.TabIndex = 3
         ' 
         ' Form4
         ' 
         AutoScaleDimensions = New SizeF(120F, 120F)
         AutoScaleMode = AutoScaleMode.Dpi
         ClientSize = New Size(1545, 910)
+        Controls.Add(btnTicket)
+        Controls.Add(btnBooking)
+        Controls.Add(lblMainBooking)
         MaximumSize = New Size(1563, 957)
         Name = "Form4"
         StartPosition = FormStartPosition.CenterScreen
         Text = "Form4"
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents btnTicket As Button
+    Friend WithEvents btnBooking As Button
+    Friend WithEvents lblMainBooking As Label
 End Class
