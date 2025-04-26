f0aef24d37df234d0439de56ac56acb7a5a965e2
Kyran <kyransolomon5@gmail.com>
2025-04-26 20:12:37 +0800

    MySql, remote access database error

--- admin/Module1.vb
+++ admin/Module1.vb
@@ -5,7 +5,7 @@ Module Module1
     Public cmd As New MySqlCommand
 
     Sub openCon()
-        con.ConnectionString = "server=100,94,231,52; username=kelvin; password=; database=testing_db"
+        con.ConnectionString = "server=26.143.215.198; username=ky; password=ky; database=testing_db"
         con.Open()
     End Sub
 
