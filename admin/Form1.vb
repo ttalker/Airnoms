267e1c392ae03c079354b211029bc78ff2f4db1d
Kyran <kyransolomon5@gmail.com>
2025-04-27 19:14:07 +0800

    changes in module admin

--- admin/Form1.vb
+++ admin/Form1.vb
@@ -1,12 +1,14 @@
 ﻿Public Class Form1
-    'Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-    '    Try
-    '        openCon()
-    '        MessageBox.Show("Connection Successful!")
-    '    Catch ex As Exception
-    '        MessageBox.Show("Connection Failed: " & ex.Message)
-    '    Finally
-    '        con.Close()
-    '    End Try
-    'End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+            openCon()
+            MessageBox.Show("Connection Successful!")
+        Catch ex As Exception
+            MessageBox.Show("Connection Failed: " & ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+
 End Class
--- admin/Module1.vb
+++ admin/Module1.vb
@@ -5,7 +5,7 @@ Module Module1
     Public cmd As New MySqlCommand
 
     Sub openCon()
-        con.ConnectionString = "server=26.143.215.198; username=ky; password=ky; database=testing_db"
+        con.ConnectionString = "server=100.89.19.71; username=root; password=; database=testing_db"
         con.Open()
     End Sub
 
