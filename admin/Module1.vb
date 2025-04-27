09c7490dd4b2768e5035d8cb8a15c041ef849695
Kelvin Rivera <kelvindaverivera26@gmail.com>
2025-04-27 14:10:42 +0800

    fixed the connection issue

--- admin/Module1.vb
+++ admin/Module1.vb
@@ -4,10 +4,15 @@ Module Module1
     Public con As New MySqlConnection
     Public cmd As New MySqlCommand
 
+    'Recommend to change method name based on user access
     Sub openCon()
-        con.ConnectionString = "server=100,94,231,52; username=kelvin; password=; database=testing_db"
+        con.ConnectionString = "server=100.113.245.127; username=kelv; password=kel; database=kyky"
         con.Open()
     End Sub
+
+    'Make methods for update, select, and delete for code reuse 
+
+
 
 End Module
 
