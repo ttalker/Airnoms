0ba6b83384299f5713430e94dfb885df2ede51e8
Kyran <kyransolomon5@gmail.com>
2025-04-28 20:30:23 +0800

    changes in error provider of form 2

--- userform/Form2.vb
+++ userform/Form2.vb
@@ -24,14 +24,7 @@ Public Class Form2
         Me.DoubleBuffered = True
 
 
-        'Try
-        '    openCon()
-        '    MessageBox.Show("Connection Successful!")
-        'Catch ex As Exception
-        '    MessageBox.Show("Connection Failed: " & ex.Message)
-        'Finally
-        '    con.Close()
-        'End Try
+        ErrorProvider.Clear() ' clear previous error
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSignIn.Click
@@ -55,19 +48,52 @@ Public Class Form2
         email = tbxEmailSignUp.Text
     End Sub
 
+    'Dim hasError As Boolean = False
     Private Sub btnSignUp_Click(sender As Object, e As EventArgs) Handles btnSignUp.Click
+
+        Dim hasError As Boolean = False
+        ErrorProvider.Clear()
+        If String.IsNullOrWhiteSpace(tbxFullnameSignUp.Text) Then
+            ErrorProvider.SetError(tbxFullnameSignUp, "Invalid name")
+            hasError = True
+        End If
+
+        If String.IsNullOrWhiteSpace(tbxUsernameSignUp.Text) Then
+            ErrorProvider.SetError(tbxUsernameSignUp, "Invalid username")
+            hasError = True
+        End If
+
+        If String.IsNullOrWhiteSpace(tbxPasswordSignUp.Text) Then
+            ErrorProvider.SetError(tbxPasswordSignUp, "Invalid password")
+            hasError = True
+        End If
+
+        If String.IsNullOrWhiteSpace(tbxEmailSignUp.Text) Then
+            ErrorProvider.SetError(tbxEmailSignUp, "Invalid email")
+            hasError = True
+        End If
+
+        If tbxPasswordSignUp.Text.Length < 8 Then
+            ErrorProvider.SetError(tbxPasswordSignUp, "Password must be at least 8 characters long")
+            hasError = True
+        End If
+
+        If hasError = True Then
+            Exit Sub
+        End If
+
         Try
             openCon()
             'MessageBox.Show("Connection Successful!")
             cmd.Connection = con
-            cmd.CommandText = "ALTER TABLE acc_table MODIFY `user_id` INT AUTO_INCREMENT;"
+            cmd.CommandText = "ALTER TABLE acc_table MODIFY user_id INT AUTO_INCREMENT;"
             cmd.ExecuteNonQuery()
 
-            cmd.CommandText = "INSERT INTO acc_table (`Username`, `Password`, `Fullname`, `Email`) VALUES ('" &
-                          tbxUsernameSignUp.Text & "', '" &
-                          tbxPasswordSignUp.Text & "', '" &
-                          tbxFullnameSignUp.Text & "', '" &
-                          tbxEmailSignUp.Text & "')"
+            cmd.CommandText = "INSERT INTO acc_table (Username, Password, Fullname, Email) VALUES ('" &
+                   tbxUsernameSignUp.Text & "', '" &
+                   tbxPasswordSignUp.Text & "', '" &
+                   tbxFullnameSignUp.Text & "', '" &
+                   tbxEmailSignUp.Text & "')"
             cmd.ExecuteNonQuery()
 
             MessageBox.Show("User registered successfully!")
