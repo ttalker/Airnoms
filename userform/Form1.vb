25613c7caf535f6c6758c2f6b510eecd06f22faf
Kyran <kyransolomon5@gmail.com>
2025-05-01 15:48:47 +0800

    commits

--- userform/Form1.vb
+++ userform/Form1.vb
@@ -1,9 +1,6 @@
 ﻿Imports SharedModule
 Imports MySql.Data.MySqlClient
 Public Class Form1
-    'Public Property username As String
-    'Public Property password As String
-
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         MakeTransparent(btnSignIn)
@@ -16,13 +13,6 @@ Public Class Form1
         btnSignIn.FlatAppearance.MouseDownBackColor = Color.FromArgb(90, 255, 255, 255) ' effects for button
         Me.DoubleBuffered = True
     End Sub
-    'Private Sub tbxUsername_TextChanged(sender As Object, e As EventArgs) Handles tbxUsername.TextChanged
-    '    username = tbxUsername.Text
-    'End Sub
-
-    'Private Sub tbxPassword_TextChanged(sender As Object, e As EventArgs) Handles tbxPassword.TextChanged
-    '    password = tbxPassword.Text
-    'End Sub
 
     Private Sub btnCreateAccount_Click(sender As Object, e As EventArgs) Handles btnCreateAccount.Click
         Me.Hide()
