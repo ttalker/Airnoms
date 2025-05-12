222534949860fd6f6c0fae21821a870667b06fab
ACER <ACER@LAPTOP-6PCLLSMD>
2025-05-12 15:00:46 +0800

    Revert "added calculate button on ticket ui"
    This reverts commit 1dbd9f366af076edcbe6c5efa8aeb942df31673c.

--- cashierform/My Project/Resources.Designer.vb
+++ cashierform/My Project/Resources.Designer.vb
@@ -313,16 +313,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property TICKET_PAGE__1___1_() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("TICKET PAGE (1) (1)", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property TICKET_PAGE__2_() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("TICKET PAGE (2)", resourceCulture)
